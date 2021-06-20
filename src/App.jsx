f9930996498c4b677befee2f8e785ffbdd62d9be
Giuliana <giuliana.aliotti@hotmail.com>
2021-06-20 19:30:12 -0300

    Challenge: counter with button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Header } from './components/Header/index';
-import { ItemListContainer } from './components/ItemListContainer/index';
+import { Header } from './components/Header';
+import { ItemListContainer } from './components/ItemListContainer';
+import { ItemCount } from './components/ItemCount';
 
 function App() {
   return (
     <div>
       <Header />
       <ItemListContainer greeting="Hello World" />
+      <ItemCount stock="5"/>
     </div>
 
   );
---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {
+   HStack,
+   Button,
+   Input,
+   useNumberInput
+  } from "@chakra-ui/react";
+
+export const ItemCount = ({stock}) => {
+
+    const {
+        getInputProps,
+        getIncrementButtonProps,
+        getDecrementButtonProps,
+    } = useNumberInput({
+        step: 1,
+        defaultValue: 1,
+        min: 1,
+        max: stock,
+    })
+    
+    const inc = getIncrementButtonProps()
+    const dec = getDecrementButtonProps()
+    const input = getInputProps({ isReadOnly: true })
+    
+    return (
+        <HStack maxW="320px">
+            <Button {...dec}>-</Button>
+            <Input size="md" maxW={24} {...input} />
+            <Button {...inc}>+</Button>
+        </HStack>
+    )
+};
+
+
+//import { useState } from 'react';
+
+// export const ItemCount = ({stock}) => {
+    
+//     const [ counter, setCounter ] = useState(1);
+
+//     const add = () => {
+//         if (counter < stock) {
+//             setCounter(counter + 1);
+//         }
+//     }
+
+//     const subtract = () => {
+//         if (counter > 1) {
+//             setCounter(counter - 1);
+//         }
+//     }
+
+//     return (
+//         <>
+//             <button onClick={ subtract }>-</button>
+//             <input type="number" value={ counter } />
+//             <button onClick={ add }>+</button>
+//         </>
+//     )
+// };
