ab9c50e021c944ad18c943d16d9bba57128ea018
Giuliana <giuliana.aliotti@hotmail.com>
2021-08-21 21:31:39 -0300

    Create Routes on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import "./index.css";
-import { Header } from "./components/Header";
+import { NavBar } from "./components/NavBar";
 import { HeroContainer } from "./components/HeroContainer";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <NavBar />
+
         <Switch>
           <Route exact path="/">
-            <Header />
             <HeroContainer />
             <ItemListContainer />
           </Route>
 
-          <Route path="/detail">
+          <Route exact path="/shop">
+            <ItemListContainer />
+          </Route>
+
+          <Route exact path="/category/:catId">
+            <ItemListContainer />
+          </Route>
+
+          <Route exact path="/detail/:itemId">
             <ItemDetailContainer />
+          </Route>
+
+          <Route exact path="/cart">
+            <h1>Cart</h1>
+          </Route>
+
+          <Route path="*">
+            <Redirect to="/" />
           </Route>
         </Switch>
       </BrowserRouter>
