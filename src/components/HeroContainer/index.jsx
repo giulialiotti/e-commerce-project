972071ab8ccc2c39bc931b5174c2b25aa0e160a9
giuliana_aliotti <giuliana.aliotti@hotmail.com>
2021-09-04 19:22:34 -0300

    Change header styles

--- src/components/HeroContainer/index.jsx
+++ src/components/HeroContainer/index.jsx
@@ -9,16 +9,20 @@ export const HeroContainer = () => {
         className="hero-image-container"
         style={{ backgroundImage: `url(${assets.heroImage})` }}
       >
-        <div className="new-trend-text">
-          <span></span>
-          <p>New trend</p>
-        </div>
-        <h2>
-          Summer sale stylish <strong>womens</strong>
-        </h2>
-        <div className="discover-more-text">
-          <a href="#item-list-section">Discover more</a>
-          <span></span>
+        <div>
+          <div className="new-trend-text">
+            <span></span>
+            <p>New trend</p>
+          </div>
+
+          <h2>
+            Summer sale stylish <strong>womens</strong>
+          </h2>
+
+          <div className="discover-more-text">
+            <a href="#item-list-section">Discover more</a>
+            <span></span>
+          </div>
         </div>
       </div>
     </div>
