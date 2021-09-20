439c66aa1125eeb69bbc652207ee0506f7491255
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2021-09-19 21:30:57 -0300

    Checkout form control

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,39 +1,71 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CartContext } from '../../context/CartContext';
-import { getFirestore } from '../../firebase/config';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import { generateOrder } from '../../firebase/generateOrder';
+import { Button } from '@chakra-ui/button';
+import {
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    InputLeftAddon
+  } from "@chakra-ui/react"
 
 export const Checkout = () => {
+    const [formValues, setFormValues] = useState({
+        name: '',
+        lastname: '',
+        email: '',
+        phone: 0,
+    });
 
     const { cart, totalCartPrice } = useContext(CartContext);
-    
-    const db = getFirestore();
-    const orders = db.collection('orders');
 
-    const buyer = {
-        name: 'Name and Lastname',
-        phone: 11123950003,
-        email: 'example@gmail.com'
+    const handleInputChange = (e) => {
+        setFormValues({
+            ...formValues,
+            [e.target.name]: e.target.value
+        })
     };
 
-    const newOrder = {
-        buyer: buyer,
-        items: cart,
-        total: totalCartPrice(),
-        date: firebase.firestore.Timestamp.fromDate(new Date())
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(cart);
+        console.log(formValues);
+        // Add: generate order only if form values are correct
+        generateOrder(formValues, cart, totalCartPrice());
     };
 
-    try {
-        orders.add(newOrder)
-            .then((res) => console.log(res));        
-    } catch (error) {
-        console.log(error)
-    }
+    console.log('cart', cart)
 
     return (
-        <div>
+        <Box p='5% 10%'>
             <h1>Checkout</h1>
-        </div>
+            {/* Add cart info */}
+            {!cart.length 
+                ? <h3>There's no items on the cart</h3>
+                : 
+                <FormControl width={['100%', '40%']} isRequired>
+                    <FormLabel mt='5'>First name</FormLabel>
+                    <Input placeholder="First name" type='text' name='name' value={formValues.name} onChange={handleInputChange} />                    
+                    
+                    <FormLabel mt='5'>Last name</FormLabel>
+                    <Input placeholder="Last name" type='text' name='lastname' value={formValues.lastname} onChange={handleInputChange} />
+                    
+                    <FormLabel mt='5'>Email address</FormLabel>
+                    <Input placeholder="Your email" type="email" name='email' value={formValues.email} onChange={handleInputChange}/>
+                    
+                    <FormLabel mt='5'>Phone number</FormLabel>
+                    <InputGroup>
+                        <InputLeftAddon children="+234" />
+                        <Input type="tel" placeholder="Your phone number" name='phone' value={formValues.phone} onChange={handleInputChange} />
+                    </InputGroup>
+
+                    <Button type="submit" bg="var(--cream)" color="white" size="md" mt='5' onClick={handleSubmit}>
+                        Generate order
+                    </Button>                
+                </FormControl>
+            }
+        </Box>
     )
 }
