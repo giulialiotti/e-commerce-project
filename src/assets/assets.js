ac373eda94a4f6f608a7a69b3d589c8050e8de9d
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2021-09-26 20:15:11 -0300

    Added image

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,12 +2,14 @@ import heroImage from "./img/hero-image.jpg";
 import gridOne from "./img/grid-one.jpg";
 import gridTwo from "./img/grid-two.jpg";
 import gridThree from "./img/grid-three.jpg";
+import emptyBasket from "./img/empty-basket.jpeg";
 
 const exportable = {
   heroImage: heroImage,
   gridOne: gridOne,
   gridTwo: gridTwo,
   gridThree: gridThree,
+  emptyBasket: emptyBasket,
 };
 
 export default exportable;
